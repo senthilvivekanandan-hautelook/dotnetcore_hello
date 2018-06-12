45fa01937ac053539dde40ab590d22d69b1c5b50
senthil.vivekanandan <senthil.vivekanandan@hautelook.com>
2018-06-12 14:50:34 -0700

    updated

--- dotnetcore_hello/Program.cs
+++ dotnetcore_hello/Program.cs
@@ -6,10 +6,18 @@ namespace dotnetcore_hello
     {
         static void Main(string[] args)
         {
+            /*adding comment*/
+            Boolean answer=false;
+
+            if(answer==false)
+            { 
+
+
             for(int i=0;i<=10;i++)
             {
                 Console.WriteLine("Hello World!");
                 Console.ReadLine();
+            }
             }
         }
     }
